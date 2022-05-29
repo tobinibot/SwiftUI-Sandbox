d8e5f0bf90050714073e8f670e0a5c7d445baacf
Tobin Juday <tobinibot@gmail.com>
2022-05-29 00:33:28 -0400

    initial app
    reproduces the "empty" NavigationView issue

--- SwiftUI Sandbox/ContentView.swift
+++ SwiftUI Sandbox/ContentView.swift
@@ -1,18 +1,38 @@
-//
-//  ContentView.swift
-//  SwiftUI Sandbox
-//
-//  Created by Tobin Juday on 5/29/22.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State var selectedView = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            Color.gray
+            NavigationView {
+                List {
+                    NavigationLink("1", destination: NumberView(nbr: 1))
+                    NavigationLink("2", destination: NumberView(nbr: 2))
+                    NavigationLink("3", destination: NumberView(nbr: 3))
+                }
+            }
+            Color.indigo
+            Color.pink
+        }
+            .tabViewStyle(.page)
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+
     }
 }
+
+struct NumberView: View {
+    let nbr: Int
+    
+    var body: some View {
+        Text(String(nbr))
+            .font(.title)
+            .fontWeight(.heavy)
+    }
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
