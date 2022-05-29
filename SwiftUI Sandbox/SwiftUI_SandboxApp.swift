8d6b9852d0a63d002b24045b4fdd05c1dad3e6c4
Tobin Juday <tobinibot@gmail.com>
2022-05-29 00:03:50 -0400

    Initial Commit

---
+++ SwiftUI Sandbox/SwiftUI_SandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SandboxApp.swift
+//  SwiftUI Sandbox
+//
+//  Created by Tobin Juday on 5/29/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
